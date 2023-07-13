cb20150c7e095fd0f5680878c6ad437875f92bbd
Rae Stanton <raestanton93@gmail.com>
2023-07-13 16:35:10 -0700

    Function lab
    This adds the appropriate functions to satisfy the given test requirements for the intro to functions lab.

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase();
+};
+
+function whisper(string) {
+  return string.toLowerCase();
+};
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+  return string.toUpperCase();
+}
+
+logShout("hello");
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+  return string.toLowerCase();
+}
+
+logWhisper("HELLO");
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+}
